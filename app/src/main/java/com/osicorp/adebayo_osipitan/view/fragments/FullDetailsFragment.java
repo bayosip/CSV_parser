3071f43d4f84acb1488ec68c4f15852da4015c4b
Adebayo Osipitan <adebayo@Adebayos-MacBook-Pro.local>
2020-09-10 01:28:02 +0100

    bug fix in Full detail fragment

--- app/src/main/java/com/osicorp/adebayo_osipitan/view/fragments/FullDetailsFragment.java
+++ app/src/main/java/com/osicorp/adebayo_osipitan/view/fragments/FullDetailsFragment.java
@@ -32,7 +32,7 @@ public class FullDetailsFragment extends BaseFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_filter_selection, container, false);
+        View view = inflater.inflate(R.layout.fragment_data_detail, container, false);
         return view;
     }
 
